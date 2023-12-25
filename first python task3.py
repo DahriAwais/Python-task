a5765b7ef6c383b8a6389c653acba0d1fb1ddea4
Muhammad Awais <151646479+awais0942@users.noreply.github.com>
2023-12-25 12:47:08 +0500

    Add files via upload

---
+++ first python task3.py
@@ -0,0 +1,83 @@
+# Question no:1
+name="Muhammad Awais"
+age="19"
+print("Name is:",{name})
+print("Age is:",{age})
+
+
+#Question no:2
+
+num1=5
+num2=8
+result=num1+num2
+print(num1)
+print(num2)
+print(result)
+
+#question no 3
+
+price=23.4
+quantity=6
+total_cost=price*quantity
+print(total_cost)
+
+#questio no 4
+
+first_name="Muhammad"
+last_name="Awais"
+full_name=first_name+" "+last_name
+print(full_name)
+
+#question no 5
+
+is_adult="True"
+age=19
+print(is_adult)
+
+#question no 6
+
+name=input("Enter your name:")
+print(f"{name}")
+
+#question no 7
+
+celsius=56
+fahrenheit=celsius*9/5+32
+print(fahrenheit)
+
+#question no 8
+
+a=1
+b=2
+c=a
+a=b
+b=c
+print(f"a is{a}")
+print(f"b is{b}")
+
+# question no 9
+"""
+num1= int(input("enter a number1:"))
+num2=int(input("enter a number2:"))
+opt=input("enter operator:")
+if opt=='+,-,*,/':
+    print(f"adition is{num1+num2}")
+    print(f"subtraction is {num1-num2}")
+    print(f"multiplication is {num1*num2}")
+    print(f"division is {num1/num2}")
+"""  
+# question no 11
+
+full_name="Muhammad Awais"
+first_name="Muhammad"
+last_name="Awais"
+print(first_name)
+print(last_name)
+
+
+
+
+
+
+
+
